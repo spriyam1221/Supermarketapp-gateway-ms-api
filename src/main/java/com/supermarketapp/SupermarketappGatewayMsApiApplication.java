c26d12312c0453d4348480cc5a6f0b2638674abd
spriyam1221 <spriyam1221@gmail.com>
2022-05-06 14:44:32 +0530

    Added first feature

---
+++ src/main/java/com/supermarketapp/SupermarketappGatewayMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.supermarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketappGatewayMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketappGatewayMsApiApplication.class, args);
+		System.out.println("hai");
+	}
+
+}
---
+++ src/test/java/com/supermarketapp/SupermarketappGatewayMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketappGatewayMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
